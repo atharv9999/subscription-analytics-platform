3da69baab4e66e8804a37c09ce849e7d4525b8db
atharv <atharvk9999@gmail.com>
2026-03-04 18:37:07 +0000

    Migration script for plans table

---
+++ src/database/migrations/2026_03_04_182806_create_plans_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('plans', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+
+            $table->uuid('product_id');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+
+            $table->string('name');
+            $table->string('billing_cycle');
+
+            $table->string('pricing_model');
+            $table->decimal('base_price', 15, 2)->default(0.00);
+            $table->decimal('per_unit_price',15, 2)->nullable();
+
+            $table->string('currency', 3)->default('USD');
+            $table->integer('trial_days')->default(0);
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('plans');
+    }
+};
